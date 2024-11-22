7c3b276225340d0792f25566f996bc1d6fcd7592
shmll <shmllslmt@gmail.com>
2024-11-22 15:23:17 +0800

    Complete

--- src/Main.java
+++ src/Main.java
@@ -4,8 +4,8 @@ public class Main {
     // global variables declaration
     static int customerID;
     static int callsMade;
-    static int callsMinutes;
-    static int customerBill;
+    static int callMinutes;
+    static double customerBill;
 
     // constants declaration
     final static int CALLS = 100;
@@ -13,21 +13,65 @@ public class Main {
     final static double BASIC_SERVICE = 30.0;
     final static double PREMIUM = 20.0;
 
-    Scanner input = new Scanner(System.in);
+    static Scanner input = new Scanner(System.in);
 
     public static void housekeeping() {
+        // Jolin Tang
+        // Display welcome message
+        System.out.println("Phone Payment Calculator");
 
+        // Darshini
+        // Prompt user for customerID
+        System.out.print("Enter customer ID: ");
+
+        // Retrieve customerID
+        customerID = input.nextInt();
+
+        // Prompt user for callsMade
+        System.out.print("Enter calls made: ");
+
+        // Retrieve callsMade
+        callsMade = input.nextInt();
+
+        // Prompt user for callMinutes
+        System.out.print("Enter call minutes used: ");
+
+        // Retrieve callMinutes
+        callMinutes = input.nextInt();
     }
 
     public static void detailLoop() {
+        customerBill = BASIC_SERVICE;
 
+//        // cascading if
+//        // single alternative
+//        if(callsMade > CALLS) {
+//            // single alternative
+//            if(callMinutes > MINUTES) {
+//                customerBill += PREMIUM;
+//            }
+//        }
+
+        // compounding two expressions
+        // using and
+        if(callsMade > CALLS && callMinutes > MINUTES) {
+                customerBill += PREMIUM;
+        } else {
+            System.out.println("False");
+        }
+
+        System.out.println(customerID + " calls made used " + callMinutes + " minutes. Total bill is RM" + customerBill);
     }
 
     public static void finish() {
-
+        // Lau Li Bin
+        // Display exit message
+        System.out.println("Program ended");
     }
 
     public static void main(String[] args) {
-
+        housekeeping();
+        detailLoop();
+        finish();
     }
 }
