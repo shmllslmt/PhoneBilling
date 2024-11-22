2b931cf728034cfc73176ab16a3e7ff42170790b
shmll <shmllslmt@gmail.com>
2024-11-22 14:15:59 +0800

    Structure

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,29 @@
 public class Main {
+    // global variables declaration
+    static int customerID;
+    static int callsMade;
+    static int callsMinutes;
+    static int customerBill;
+
+    // constants declaration
+    final static int CALLS = 100;
+    final static int MINUTES = 500;
+    final static double BASIC_SERVICE = 30.0;
+    final static double PREMIUM = 20.0;
+
+    public static void housekeeping() {
+
+    }
+
+    public static void detailLoop() {
+
+    }
+
+    public static void finish() {
+
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
     }
 }
